1ba95adf554fb04903ce9ba580dea5c94cc3d503
Pounat <nathan7418@gmail.com>
2020-01-29 21:16:50 -0700

    Succesfully connected to database

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+use burgers_db;
+
+insert into burgers (burger_name, devoured)
+	values ("jammed onion", true);
+    
+insert into burgers (burger_name, devoured)
+	values ("Whiskey River BBQ", true);
+    
+insert into burgers (burger_name, devoured)
+	values ("Whopper", 0);
